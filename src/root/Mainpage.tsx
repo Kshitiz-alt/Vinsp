a8c6e587702dd4ebaa8ca0aa073319313cc50f2f
Kshitiz-alt <saxenaktz@gmail.com>
2025-07-09 21:54:46 +0530

    bug fix

--- src/root/Mainpage.tsx
+++ src/root/Mainpage.tsx
@@ -2,10 +2,25 @@ import NewTracksSub from "../Components/subPages/NewTracksSub"
 import Carousel from "../Components/subComponents/Animations/Carousel"
 import Albums from "../Components/subPages/Albums"
 import ArtistsSub from "../Components/subPages/ArtistsSub"
+import { useEffect, useRef } from "react"
 
 const Mainpage = () => {
+
+  const page = useRef<HTMLDivElement>(null)
+
+  useEffect(()=>{
+    const setHeight = () => {
+      if(page.current){
+        page.current.style.height = `${window.innerHeight}px`
+      }
+    }
+    setHeight();
+    window.addEventListener("resize",setHeight)
+    return () => window.removeEventListener("resize",setHeight)
+
+  },[])
   return (
-    <section className="h-[96.8svh] max-sm:min-w-0 w-full">
+    <section ref={page} className="h-[96.8svh] max-sm:min-w-0 w-full">
       <Carousel />
       <div className="absolute flex-col top-1/2 p-1 w-11/12 md:px-4">
           <NewTracksSub />
